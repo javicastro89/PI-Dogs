f4d9a8488708b82140501c2b4ec81b5459ca31f1
Javi <javiercastroloureiro@gmail.com>
2021-08-09 22:50:48 -0300

    api

--- client/src/Actions/actions.js
+++
@@ -1,5 +0,0 @@
-export const GET_BREEDS = 'GET_BREEDS'
-export const SEARCH_BREEDS = 'SEARCH_BREEDS'
-export const GET_BREED_DETAIL = 'GET_BREED_DETAIL'
-export const GET_TEMPERAMENT = 'GET_TEMPERAMENT'
-export const ADD_BREED = 'ADD_BREED'
